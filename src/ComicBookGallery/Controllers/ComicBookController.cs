008502c71af7fed8c2e41067b0dad9c47a717316
sylpi10 <syl.pillet@hotmail.fr>
2019-09-16 10:52:32 +0200

    added ComicBook Detail view

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -10,13 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Friday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("hello from comic book controller"); 
-          
+            return View();
         }
     }
 }
