20b35d18998271132cc6f4c8b79723eba17f8045
sylpi10 <syl.pillet@hotmail.fr>
2019-09-16 10:08:57 +0200

    added detail action method

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,5 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
+        public string Detail()
+        {
+            return "hello from comic book controller";
+        }
     }
 }
